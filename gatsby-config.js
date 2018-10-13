1420a7284d155f61a0b521d96ff75015ca514b58
Dan Homola <dan.homola@vzp.cz>
2018-10-13 14:34:45 +0200

    feat: add support for semantic ui theming

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-typescript",
+    "gatsby-plugin-less",
     {
       resolve: "gatsby-transformer-csv",
       options: {
@@ -21,12 +22,6 @@ module.exports = {
         theme_color: "#663399",
         display: "minimal-ui",
         icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: "gatsby-plugin-typography",
-      options: {
-        pathToConfigModule: "src/utils/typography",
       },
     },
     {
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Helmet from "react-helmet";
 import { StaticQuery, graphql } from "gatsby";
-import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
+import "semantic-ui-less/semantic.less";
 
 import Header from "./header";
 import Footer from "./footer";
-import { Container } from "semantic-ui-react";
 
 const Layout: React.SFC<{ title: string }> = ({ children, title }) => (
   <>
